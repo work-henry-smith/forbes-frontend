f4cec9b98422738d7ed29673c303f2972573563b
Henry Smith <work.henrysmith@gmail.com>
2022-09-17 16:43:27 +0700

    inject the page components into the root component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import './App.css';
+import HomePage from './pages/HomePage';
+import AboutPage from './pages/AboutPage';
+import ArticlesListPage from './pages/ArticlesListPage';
+import ArticlePage from './pages/ArticlePage';
+
 
 function App() {
   return (
     <div className="App">
       <h1>This is a website for sharing free economic news and more</h1>
-      <div id='page-body'>Welcome to pollos homestay</div>
+      <div id='page-body'>
+        <AboutPage />
+        <ArticlePage />
+        <ArticlesListPage />
+        <HomePage />
+      </div>
     </div>
   );
 }
