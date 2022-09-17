ee19628eaf3af965e71777c7a630145cf61e3f42
Henry Smith <work.henrysmith@gmail.com>
2022-09-17 22:21:14 +0700

    make the links of the articles more reusable

--- src/pages/ArticlesListPage.js
+++ src/pages/ArticlesListPage.js
@@ -1,16 +1,11 @@
 import articles from './article-content';
-import {Link} from 'react-router-dom';
+import ArticlesList from '../components/ArticlesList';
 
 const ArticlesListPage = () => {
     return (
         <>
             <h1> Danh sách bài viết</h1>
-            {articles.map(article => (
-                <Link key={article.name} className='article-list-item' to={`/articles/${article.name}`}>
-                    <h3>{article.title}</h3>
-                    <p>{article.content[0].substring(0,150)}...</p>
-                </Link>
-            ))}
+            <ArticlesList articles={articles} />
         </>
     )
 }
