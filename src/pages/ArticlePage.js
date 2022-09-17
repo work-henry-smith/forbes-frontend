49f7a105c87c006922781c0cc50fbfa6489d4e4e
Henry Smith <work.henrysmith@gmail.com>
2022-09-17 16:35:12 +0700

    create three-page components

---
+++ src/pages/ArticlePage.js
@@ -0,0 +1,7 @@
+const ArticlePage = () => {
+    return (
+        <h1>Chi tiết bài viết</h1>
+    )
+}
+
+export default ArticlePage;
---
+++ src/pages/ArticlesListPage.js
@@ -0,0 +1,7 @@
+const ArticlesListPage = () => {
+    return (
+        <h1> Danh sách bài viết</h1>
+    )
+}
+
+export default ArticlesListPage;
