bd51af02a476f3cd7d3a7fa9dbb1292adfa49b97
Henry Smith <work.henrysmith@gmail.com>
2022-09-17 20:42:51 +0700

    associate the route, uesParams in URL parameters

--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -1,6 +1,18 @@
+import {useParams} from 'react-router-dom';
+import articles from './article-content';
+
 const ArticlePage = () => {
+    const {articleId} = useParams();
+    const article = articles.find(article => article.name === articleId);
+
     return (
-        <h1>Chi tiết bài viết</h1>
+        <>
+            <h1>{article.title}</h1>
+            <h3>{article.subtitle}</h3>
+            {article.content.map(paragraph => (
+                <p>{paragraph}</p>
+            ))}
+        </>
     )
 }
 
--- src/pages/article-content.js
+++ src/pages/article-content.js
@@ -2,10 +2,9 @@ const articles = [
     {
         name: 'y-kien',
         title: 'Ý Kiến Bạn Đọc',
+        subtitle: `Những bên bán hàng thứ ba của Amazon`,
         content: [
-            `Những bên bán hàng thứ ba của Amazon`,
-            `Tiêu đề "Kiếm triệu đô từ tỉ đô của Bezos" trên số báo tháng 4.2021 đã gây tò mò.`,
-            `Bức tranh về thị trường thương mại điện tử lớn nhất thế giới, nơi không chỉ có quy mô thị trường và khối lượng sản phẩm giao dịch mang lại giá trị lợi nhuận, mà khi nó đạt đến một quy mô nhất định thì ngay chính sự khai thác trong quá trình của chuỗi cung ứng cũng có thể kiếm ra bộn tiền. Những người có lợi là người tiên phong áp dụng công nghệ và phần mềm để quản lý và tối ưu quy trình vận hành và giao dịch, cũng như các phương thức của marketing hiện đại phát huy hiệu quả khi tăng lượng khách hàng tương tác.`,
+            `Tiêu đề "Kiếm triệu đô từ tỉ đô của Bezos" trên số báo tháng 4.2021 đã gây tò mò. Bức tranh về thị trường thương mại điện tử lớn nhất thế giới, nơi không chỉ có quy mô thị trường và khối lượng sản phẩm giao dịch mang lại giá trị lợi nhuận, mà khi nó đạt đến một quy mô nhất định thì ngay chính sự khai thác trong quá trình của chuỗi cung ứng cũng có thể kiếm ra bộn tiền. Những người có lợi là người tiên phong áp dụng công nghệ và phần mềm để quản lý và tối ưu quy trình vận hành và giao dịch, cũng như các phương thức của marketing hiện đại phát huy hiệu quả khi tăng lượng khách hàng tương tác.`,
             `Cách mạng công nghiệp 4.0 đã sản sinh nhiều khái niệm mới như biến Google, Twitter thành những động từ, thì nay khái niệm “bất động sản kỹ thuật số" để chỉ sự mua bán không gian trên các nền tảng thương mại điện tử đang mang lại siêu lợi nhuận. Hiện tượng này xuất hiện trong vòng bốn, năm năm trở lại đây trên để chế thương mại lớn nhất thế giới Amazon của Bezos, đặc biệt là từ sau đại dịch COVID-19. Những người tiêu dùng trẻ tại Việt Nam đang có xu hướng tiêu dùng online, lựa chọn mua sắm trên Shoppee, Tiki, Lazada thay thế cho chợ và siêu thị truyền thống, và dân bớt sử dụng tiền mặt cũng sẽ là mảnh đất đầy hứa hẹn cho những nhà đầu tư tìm kiếm những cơ hội kiếm tiền tuyệt vời này. Tuy nhiên, với quy mô thị trường chưa đạt tới mức tối ưu cũng như các nhà bán lẻ thứ ba trên các nền tảng điện tử chủ yếu là cá nhân và hộ gia đình tiểu thương như của Việt Nam hiện tại vẫn chưa thời điểm đủ độ chín để sinh ra những “hiệp sĩ trắng” Thrasio như vậy.`,
             `Lê My Khuyên, giảng viên khoa Tài Chính, ĐH Ngân hàng TP.HCM`
         ]
