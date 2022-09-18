d259409524845a0a4b1791e8e544e26298184e72
Henry Smith <work.henrysmith@gmail.com>
2022-09-18 09:27:44 +0700

    cập nhật about page and home page

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,6 +1,12 @@
 const AboutPage = () => {
     return (
-        <h1>About us</h1>
+        <>
+            <h1>About us</h1>
+            <p>Chuyên tổng hợp những bài viết hay về kinh doanh và cuộc sống.</p>
+            <p> Cảm ơn ban biên tập Forbes Việt Nam đã xuất bản ra những nội dung chất lượng, những bài viết chuyên sâu, những góc nhìn toàn diện. Forbes thực sự đã chiếm một vị trí đặc biệt trong cộng đồng khởi nghiệp Việt Nam.</p>
+            <p>Xin chân thành cảm ơn!</p>
+            <p>Hoạt động vì cộng đồng và phi lợi nhuận. Luôn luôn là như thế!</p>
+        </>
     )
 }
 
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,6 +1,10 @@
 const HomePage = () => {
     return (
-        <h1>Welcome to Pollos website!</h1>
+        <>
+            <h1>Welcome to Pollos website!</h1>
+            <h3>Những bài viết nổi bật</h3>
+            <p>Dưới đây là những bài viết tốt nhất, được phân loại theo tháng và năm. Vui lòng chọn từ khóa và enjoy. Xin cảm ơn!</p>
+        </>
     )
 }
 
